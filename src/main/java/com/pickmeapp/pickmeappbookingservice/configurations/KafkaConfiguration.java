08be9d9d6d50c8ab92dfcb914b7011ae537d8a97
Rajat Pathak <rajatpathak9888@gmail.com>
2025-04-16 05:14:34 +0530

    adding docker file

--- src/main/java/com/pickmeapp/pickmeappbookingservice/configurations/KafkaConfiguration.java
+++ src/main/java/com/pickmeapp/pickmeappbookingservice/configurations/KafkaConfiguration.java
@@ -1,6 +1,9 @@
 package com.pickmeapp.pickmeappbookingservice.configurations;
 
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -17,10 +20,21 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConfiguration {
+
+
+    @Value("${KAFKA_SERVER}")
+    private String kafkaServerUrl;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin(){
+        Map<String,Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,kafkaServerUrl);
+        return new KafkaAdmin(configs);
+    }
     @Bean
     public Map<String,Object> consumerConfig(){
         Map<String,Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,kafkaServerUrl);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
